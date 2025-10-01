fcfa27967fa06096239f9f5074df61e6983504d7
Mathieu Ponal <mathieu.ponal@gmail.com>
2025-10-01 12:03:54 +0200

    basic start

---
+++ src/jvn/JvnObjectImpl.java
@@ -0,0 +1,63 @@
+package jvn;
+
+import java.io.Serializable;
+
+public class JvnObjectImpl implements JvnObject {
+
+    private static final long serialVersionUID = 1L;
+    private Serializable object;
+    private int objectId;
+    private boolean isReadLocked = false;
+    private boolean isWriteLocked = false;
+
+    public JvnObjectImpl(Serializable o) {
+        this.object = o;
+    }
+
+    @Override
+    public void jvnLockRead() throws JvnException {
+        if (isWriteLocked) {
+            throw new JvnException("Object is write-locked");
+        }
+        isReadLocked = true;
+    }
+
+    @Override
+    public void jvnLockWrite() throws JvnException {
+        if (isReadLocked || isWriteLocked) {
+            throw new JvnException("Object is already locked");
+        }
+        isWriteLocked = true;
+    }
+
+    @Override
+    public void jvnUnLock() throws JvnException {
+        isReadLocked = false;
+        isWriteLocked = false;
+    }
+
+    @Override
+    public int jvnGetObjectId() throws JvnException {
+        return 0;
+    }
+
+    @Override
+    public Serializable jvnGetSharedObject() throws JvnException {
+        return object;
+    }
+
+    @Override
+    public void jvnInvalidateReader() throws JvnException {
+
+    }
+
+    @Override
+    public Serializable jvnInvalidateWriter() throws JvnException {
+        return null;
+    }
+
+    @Override
+    public Serializable jvnInvalidateWriterForReader() throws JvnException {
+        return null;
+    }
+}
--- src/jvn/JvnServerImpl.java
+++ src/jvn/JvnServerImpl.java
@@ -11,148 +11,173 @@ package jvn;
 
 import java.rmi.server.UnicastRemoteObject;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
+public class JvnServerImpl
+        extends UnicastRemoteObject
+        implements JvnLocalServer, JvnRemoteServer {
 
-public class JvnServerImpl 	
-              extends UnicastRemoteObject 
-							implements JvnLocalServer, JvnRemoteServer{ 
-	
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	// A JVN server is managed as a singleton  
-	private static JvnServerImpl js = null;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    // A JVN server is managed as a singleton
+    private static JvnServerImpl js = null;
 
-  /**
-  * Default constructor
-  * @throws JvnException
-  **/
-	private JvnServerImpl() throws Exception {
-		super();
-		// to be completed
-	}
-	
-  /**
-    * Static method allowing an application to get a reference to 
-    * a JVN server instance
-    * @throws JvnException
-    **/
-	public static JvnServerImpl jvnGetServer() {
-		if (js == null){
-			try {
-				js = new JvnServerImpl();
-			} catch (Exception e) {
-				return null;
-			}
-		}
-		return js;
-	}
-	
-	/**
-	* The JVN service is not used anymore
-	* @throws JvnException
-	**/
-	public  void jvnTerminate()
-	throws jvn.JvnException {
-    // to be completed 
-	} 
-	
-	/**
-	* creation of a JVN object
-	* @param o : the JVN object state
-	* @throws JvnException
-	**/
-	public  JvnObject jvnCreateObject(Serializable o)
-	throws jvn.JvnException { 
-		// to be completed 
-		return null; 
-	}
-	
-	/**
-	*  Associate a symbolic name with a JVN object
-	* @param jon : the JVN object name
-	* @param jo : the JVN object 
-	* @throws JvnException
-	**/
-	public  void jvnRegisterObject(String jon, JvnObject jo)
-	throws jvn.JvnException {
-		// to be completed 
-	}
-	
-	/**
-	* Provide the reference of a JVN object beeing given its symbolic name
-	* @param jon : the JVN object name
-	* @return the JVN object 
-	* @throws JvnException
-	**/
-	public  JvnObject jvnLookupObject(String jon)
-	throws jvn.JvnException {
-    // to be completed 
-		return null;
-	}	
-	
-	/**
-	* Get a Read lock on a JVN object 
-	* @param joi : the JVN object identification
-	* @return the current JVN object state
-	* @throws  JvnException
-	**/
-   public Serializable jvnLockRead(int joi)
-	 throws JvnException {
-		// to be completed 
-		return null;
+    // store for JVN objects
+    private Map<String, JvnObject> objectMap = new HashMap<>();
 
-	}	
-	/**
-	* Get a Write lock on a JVN object 
-	* @param joi : the JVN object identification
-	* @return the current JVN object state
-	* @throws  JvnException
-	**/
-   public Serializable jvnLockWrite(int joi)
-	 throws JvnException {
-		// to be completed 
-		return null;
-	}	
+    /**
+     * Default constructor
+     *
+     * @throws JvnException
+     **/
+    private JvnServerImpl() throws Exception {
+        super();
+        // to be completed
+    }
 
-	
-  /**
-	* Invalidate the Read lock of the JVN object identified by id 
-	* called by the JvnCoord
-	* @param joi : the JVN object id
-	* @return void
-	* @throws java.rmi.RemoteException,JvnException
-	**/
-  public void jvnInvalidateReader(int joi)
-	throws java.rmi.RemoteException,jvn.JvnException {
-		// to be completed 
-	};
-	    
-	/**
-	* Invalidate the Write lock of the JVN object identified by id 
-	* @param joi : the JVN object id
-	* @return the current JVN object state
-	* @throws java.rmi.RemoteException,JvnException
-	**/
-  public Serializable jvnInvalidateWriter(int joi)
-	throws java.rmi.RemoteException,jvn.JvnException { 
-		// to be completed 
-		return null;
-	};
-	
-	/**
-	* Reduce the Write lock of the JVN object identified by id 
-	* @param joi : the JVN object id
-	* @return the current JVN object state
-	* @throws java.rmi.RemoteException,JvnException
-	**/
-   public Serializable jvnInvalidateWriterForReader(int joi)
-	 throws java.rmi.RemoteException,jvn.JvnException { 
-		// to be completed 
-		return null;
-	 };
+    /**
+     * Static method allowing an application to get a reference to
+     * a JVN server instance
+     *
+     * @throws JvnException
+     **/
+    public static JvnServerImpl jvnGetServer() {
+        if (js == null) {
+            try {
+                js = new JvnServerImpl();
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return js;
+    }
+
+    /**
+     * The JVN service is not used anymore
+     *
+     * @throws JvnException
+     **/
+    public void jvnTerminate()
+            throws jvn.JvnException {
+        // to be completed
+    }
+
+    /**
+     * creation of a JVN object
+     *
+     * @param o : the JVN object state
+     * @throws JvnException
+     **/
+    public JvnObject jvnCreateObject(Serializable o) throws jvn.JvnException {
+        if (o == null)
+            throw new JvnException("jvnCreateObject: null object");
+        return new JvnObjectImpl(o);
+    }
+
+    /**
+     * Associate a symbolic name with a JVN object
+     *
+     * @param jon : the JVN object name
+     * @param jo  : the JVN object
+     * @throws JvnException
+     **/
+    public void jvnRegisterObject(String jon, JvnObject jo) throws jvn.JvnException {
+        if (jon == null || jon.isEmpty())
+            throw new JvnException("jvnRegisterObject: null name");
+        if (jo == null)
+            throw new JvnException("jvnRegisterObject: null object");
+        objectMap.put(jon, jo);
+    }
+
+    /**
+     * Provide the reference of a JVN object beeing given its symbolic name
+     * If the JVN object is not found, returns null
+     * @param jon : the JVN object name
+     * @return the JVN object
+     * @throws JvnException
+     **/
+    public JvnObject jvnLookupObject(String jon) throws jvn.JvnException {
+        if (jon == null || jon.isEmpty())
+            throw new JvnException("jvnLookupObject: null name");
+        return objectMap.get(jon);
+    }
+
+    /**
+     * Get a Read lock on a JVN object
+     *
+     * @param joi : the JVN object identification
+     * @return the current JVN object state
+     * @throws JvnException
+     **/
+    public Serializable jvnLockRead(int joi)
+            throws JvnException {
+        // to be completed
+        return null;
+
+    }
+
+    /**
+     * Get a Write lock on a JVN object
+     *
+     * @param joi : the JVN object identification
+     * @return the current JVN object state
+     * @throws JvnException
+     **/
+    public Serializable jvnLockWrite(int joi)
+            throws JvnException {
+        // to be completed
+        return null;
+    }
+
+
+    /**
+     * Invalidate the Read lock of the JVN object identified by id
+     * called by the JvnCoord
+     *
+     * @param joi : the JVN object id
+     * @return void
+     * @throws java.rmi.RemoteException,JvnException
+     **/
+    public void jvnInvalidateReader(int joi)
+            throws java.rmi.RemoteException, jvn.JvnException {
+        // to be completed
+    }
+
+    ;
+
+    /**
+     * Invalidate the Write lock of the JVN object identified by id
+     *
+     * @param joi : the JVN object id
+     * @return the current JVN object state
+     * @throws java.rmi.RemoteException,JvnException
+     **/
+    public Serializable jvnInvalidateWriter(int joi)
+            throws java.rmi.RemoteException, jvn.JvnException {
+        // to be completed
+        return null;
+    }
+
+    ;
+
+    /**
+     * Reduce the Write lock of the JVN object identified by id
+     *
+     * @param joi : the JVN object id
+     * @return the current JVN object state
+     * @throws java.rmi.RemoteException,JvnException
+     **/
+    public Serializable jvnInvalidateWriterForReader(int joi)
+            throws java.rmi.RemoteException, jvn.JvnException {
+        // to be completed
+        return null;
+    }
+
+    ;
 
 }
 
